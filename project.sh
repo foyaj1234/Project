7e0ffcea5dd8a107c85f1916ce4ac4eac1d0721b
foyaj1234 <foyajislam12345@gmail.com>
2025-02-26 02:00:39 +0600

    Project.sh
    My first project for termux, any problem dm me in tg : @fbwork3

---
+++ project.sh
@@ -0,0 +1,122 @@
+clear
+pkg update && upgrade -y
+echo "termux update and upgrade success" 
+pkg install figlet -y 
+pkg install git -y
+pip install lolcat -y
+clear
+echo "✅ figlet install success"
+echo ""
+sleep 1 
+echo "✅ lolcat  install success"
+sleep 2
+echo "git install success"
+clear 
+figlet   SIGMA  | lolcat 
+
+echo ""
+echo ""
+echo ""
+echo "
+  1.Make Banner
+  2.Calculate
+  3.ip tracker
+"
+read -p "choose a number : " nam
+echo ""
+
+case $nam in
+1)
+clear
+sleep 2
+echo "❌ your banner is not parmanent"
+sleep 1
+echo "
+
+██████╗ ██╗██████╗ ██████╗ ██████╗
+██╔══██╗██║██╔══██╗╚════██╗██╔══██╗
+██║  ██║██║██████╔╝ █████╔╝██████╔╝
+██║  ██║██║██╔═══╝  ╚═══██╗██╔══██╗
+██████╔╝██║██║     ██████╔╝██║  ██║
+╚═════╝ ╚═╝╚═╝     ╚═════╝ ╚═╝  ╚═╝
+
+<=========(made by friend)==========>
+
+" | lolcat 
+;;
+2)
+clear
+ figlet Calculat | lolcat
+
+echo ""
+echo ""
+echo ""
+ echo "
+    a.calculat...
+
+    b.namta...
+"
+read -p "choose one option... " ba
+case $ba in 
+a)
+
+clear 
+figlet C A L | lolcat
+echo ""
+echo ""
+echo "Select operation:"
+    echo "1. Add"
+    echo "2. Subtract"
+    echo "3. Multiply"
+    echo "4. Divide"
+echo ""
+echo ""
+read -p"inter a number:" num1
+echo ""
+read -p "inter another number:" num2
+echo ""
+echo ""
+echo ""    
+echo "your add is:"$((num1+num2))
+
+    echo ""
+echo "your Subtract is :"$((num1-num2))
+echo ""
+   
+echo "your Divide  is:"$((num1/num2))
+echo ""
+   
+echo "your Multiply is :"$((num1*num2))
+echo ""
+
+    
+  
+
+;;
+ b)
+clear
+figlet Namta | lolcat
+echo ""
+echo ""
+echo ""
+echo ""
+read -p "  inter a number for namta :" ban
+for ((a=1;a<11;a=a+1)){
+echo ""
+echo "$ban * $a = $((ban*a))"
+}
+echo ""
+;;
+
+esac
+;;
+3)
+git clone https://github.com/rajkumardusad/IP-Tracer
+cd IP-Tracer
+bash install
+;;
+*)
+echo "sorry, some error... 😫"
+;;
+esac
+
